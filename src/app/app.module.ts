ca86611c532ef4fdd09675c6022867a7a37c50d2
Islam78 <sm09681130@gmail.com>
2021-08-26 01:30:50 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { AddTaskComponent } from './component/add-task/add-task.component';
+import { ErrComponent } from './component/err/err.component';
+import { TaskComponent } from './component/task/task.component';
+import { FormsModule } from '@angular/forms';
+import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddTaskComponent,
+    ErrComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
